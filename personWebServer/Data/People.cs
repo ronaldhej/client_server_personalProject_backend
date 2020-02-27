295db9bf94ba4c87162bee84af5e6bc303366276
ronald <ronaldhej@gmail.com>
2020-02-27 11:45:44 +0100

    Initial commit

---
+++ personWebServer/Data/People.cs
@@ -0,0 +1,11 @@
+using System;
+namespace personWebServer.Data
+{
+    public class People
+    {
+        
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int personId { get; set; }
+    }
+}
---
+++ personWebServer/Data/PeopleService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace personWebServer.Data
+{
+    public class PeopleService
+    {
+        private static readonly String[] FirstNames = new[]
+        {
+            "Dave", "Phil", "Andy", "Todd", "Ronald"
+        };
+        
+        private static readonly String[] LastNames = new[]
+        {
+            "Zhong", "Rasmussen", "Scott", "Pedersen", "Johnson"
+        };
+        
+        public Task<People[]> GetPeopleAsync()
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new People
+            {
+                FirstName = FirstNames[rng.Next(FirstNames.Length)],
+                LastName = LastNames[rng.Next(LastNames.Length)],
+                personId = rng.Next(0, 100)
+            }).ToArray());
+        }
+    }
+}
